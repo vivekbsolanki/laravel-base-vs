c17d064f93ac5287115f2e46c1eff5d59e0ddaa8
Vivek Solanki <vivekbsolanki@gmail.com>
2019-06-04 08:22:52 +0530

    Simple Listing Set

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -49,6 +49,10 @@
                                 </li>
                             @endif
                         @else
+                            <a id="navbarDropdown" class="nav-link " href="{{ route('users.index') }}">
+                                    Users <span class="caret"></span>
+                            </a>
+                                
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+		<table class="table table-striped table-bordered">
+			<tr>
+				<th>Id</th>
+				<th>Name</th>
+				<th>Email</th>
+			</tr>				
+			@foreach($users as $user)
+				<tr>
+					<td>{{ $user->id }}</td>
+					<td>{{ $user->name }}</td>
+					<td>{{ $user->email }}</td>
+				</tr>
+			@endforeach
+		</table>
+	 </div>
+</div>
+@endsection
